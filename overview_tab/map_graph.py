da508987176227d36c14e156906a276ca9a1d282
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-07 17:36:29 +0200

    adding title

--- overview_tab/map_graph.py
+++ overview_tab/map_graph.py
@@ -2,6 +2,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
+FAMILY = "PT Sans"
 
 def data_processing_for_graph(df, postal_code_df):
     """
@@ -25,16 +26,20 @@ def map_graph(df, postal_code_df):
                             lat="latitude",
                             lon="longitude",
                             size="first_time_exprience",  # size of the dots
-                            # text = "first_time_exprience",
                             color_discrete_sequence=['#d80052'],  # dots are pink
                             zoom=4.2,  # add a zoom of size of great britain
                             width=600,  # width of the graph
                             height=630,  # height of the graph
                             hover_name="first_time_exprience")
 
-    fig.update_layout(mapbox_style="carto-positron",
+    fig.update_layout(mapbox_style="carto-positron", # Chooses the type of map in the background
                       paper_bgcolor='rgba(0,0,0,0)',
                       plot_bgcolor='rgba(0,0,0,0)',
+                      title=dict(text="Number of incidents reported per postal code",
+                                 x=0.5,
+                                 font=dict(family=FAMILY,
+                                           color="white",
+                                           size=24)),
                       mapbox= dict(center=go.layout.mapbox.Center(lat=54.237933, lon=-2.36967)))
 
     return fig
