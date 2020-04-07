f9370297ae5adc6b2b457120c4c7d8dea1bc7d4f
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-07 17:59:59 +0200

    correction small error

--- overview_tab/overview.py
+++ overview_tab/overview.py
@@ -2,7 +2,7 @@ import dash_core_components as dcc
 from overview_tab.map_graph import  map_graph
 
 
-def create_overview_tab(df, postal_code_df):
+def create_overview_tab(df, postal_code_df, threshold):
     tab_content = dcc.Graph(
         figure= map_graph(df, postal_code_df, threshold)
     )
