e94a9558483928a94c6717817197f8061366a507
Roberta Conrad <rjcnrd@gmail.com>
2020-04-07 10:20:52 +0200

    added styling for tabs and background for body

--- layout.py
+++ layout.py
@@ -6,26 +6,46 @@ from overview_tab.overview import create_overview_tab
 from statistics_tab.statistics import create_statistics_tab
 from testimonials_tab.testimonials import create_testimonials_tab
 
+TAB_STYLE = {
+    'font-family': 'Arial, Helvetica, sans-serif',
+    'font-size': '3vw',
+    'background': '#00a0dd',
+    'color': 'white',
+    'border': 'none',
+    'border-top': 'none',
+}
+
+SELECTED_STYLE = {
+    'font-family': 'Arial, Helvetica, sans-serif',
+    'font-size': '3vw',
+    'background': '#d80052',
+    'color': 'white',
+    'border-top': 'none',
+    'border': 'none',
+}
+
 
 def create_layout():
     layout = html.Div(style={
-        'background': "white"
     }, children=[
         html.Div(children=[
             html.H1(children='Hello World')
         ]),
 
-        dcc.Tabs([
-            dcc.Tab(label='Overview', children=[
-                create_overview_tab()]),
+        dcc.Tabs(className="tabs", children=[
+            dcc.Tab(label='OVERVIEW', children=[
+                create_overview_tab()],
+                style=TAB_STYLE,
+                selected_style=SELECTED_STYLE),
 
-            dcc.Tab(label='Statistics', children=[
+            dcc.Tab(label='STATISTICS', children=[
                 create_statistics_tab()
-            ]),
-            dcc.Tab(label='Testimonials', children=[
+            ], style=TAB_STYLE,
+                selected_style=SELECTED_STYLE),
+            dcc.Tab(label='TESTIMONIALS', children=[
                 create_testimonials_tab()
-            ]),
+            ], style=TAB_STYLE,
+                selected_style=SELECTED_STYLE),
         ])
     ])
     return layout
-
