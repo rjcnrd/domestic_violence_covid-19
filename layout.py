79e703c81cd8b626cff6fe6f33e7def615a94a4b
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-07 17:03:10 +0200

    some changes on the layout

--- layout.py
+++ layout.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 
 from overview_tab.overview import create_overview_tab
 from statistics_tab.statistics import create_statistics_tab
