64ebb766211d3b312c27cb28b2940c5a312349e6
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-07 21:29:03 +0200

    adding graphs to the overview tab

--- overview_tab/overview.py
+++ overview_tab/overview.py
@@ -1,9 +1,10 @@
 import dash_core_components as dcc
-from overview_tab.map_graph import  map_graph
+from overview_tab.map_graph import map_graph
+import dash_html_components as html
+from overview_tab.cumulative_nb_report import cumulative_graph
 
 
 def create_overview_tab(df, postal_code_df, threshold):
-    tab_content = dcc.Graph(
-        figure= map_graph(df, postal_code_df, threshold)
-    )
+    tab_content = html.Div([dcc.Graph(figure=map_graph(df, postal_code_df, threshold)),
+                            dcc.Graph(figure=cumulative_graph(df))])
     return tab_content
