4881b9d172fbaa4b3d33007f06c7cd8a7900af37
Roberta Conrad <rjcnrd@gmail.com>
2020-04-07 17:42:09 +0200

    created first div for title of chart

--- statistics_tab/statistics.py
+++ statistics_tab/statistics.py
@@ -6,8 +6,13 @@ import plotly.graph_objects as go
 
 from statistics_tab.scatterplot_per_day import draw_scatterplot_per_day 
 
+
 def create_statistics_tab(df):
-    tab_content = dcc.Graph(
-        figure = draw_scatterplot_per_day(df)
-    )
+    tab_content = [
+        html.Div(children='Hello Dash'),
+        dcc.Graph(
+            figure=draw_scatterplot_per_day(df),
+            responsive=True
+        )]
     return tab_content
+
