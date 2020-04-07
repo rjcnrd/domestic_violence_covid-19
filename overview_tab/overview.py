2079d44620165ed86929324f6e7c8b399e65f9d6
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-07 22:16:18 +0200

    put cumulative chart next to map

--- overview_tab/overview.py
+++ overview_tab/overview.py
@@ -5,6 +5,13 @@ from overview_tab.cumulative_nb_report import cumulative_graph
 
 
 def create_overview_tab(df, postal_code_df, threshold):
-    tab_content = html.Div([dcc.Graph(figure=map_graph(df, postal_code_df, threshold)),
-                            dcc.Graph(figure=cumulative_graph(df))])
+    tab_content = html.Div([
+        html.Div([
+            dcc.Graph(figure=map_graph(df, postal_code_df, threshold)),
+            ],
+            style={'width': '48%', 'display': 'inline-block'}),
+        html.Div([dcc.Graph(figure=cumulative_graph(df))],
+                 style={'width': '48%', 'align': 'right', 'display': 'inline-block'})])
     return tab_content
+
+
