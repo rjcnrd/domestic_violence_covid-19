7d4eb5200a70edc63de4eb3e97682c2791dcfd49
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-08 15:30:39 +0200

    adapt graph to number new data set

--- overview_tab/cumulative_nb_report.py
+++ overview_tab/cumulative_nb_report.py
@@ -10,8 +10,11 @@ def data_processing_cumulative(survey_df):
     :return: a pandas data frame with the cumulative number of report per day
     """
     survey_df.date_of_report = pd.to_datetime(survey_df.date_of_report, format='%d/%m/%Y %I:%M %p')
-    data = pd.DataFrame(survey_df.groupby("date_of_report")["first_time_experience"].count().cumsum())
+    survey_df["date_of_report_day"] = survey_df.date_of_report.dt.strftime('%Y/%m/%d')
+    survey_df = survey_df.sort_values(by="date_of_report_day")
+    data = pd.DataFrame(survey_df.groupby("date_of_report_day")["first_time_experience"].count().cumsum())
     data = data.rename(columns={"first_time_experience": "cumulative_number_of_reports"}).reset_index()
+    data.date_of_report_day = pd.to_datetime(data.date_of_report_day)
     return data
 
 
@@ -22,7 +25,7 @@ def cumulative_graph(survey_df):
     """
     data = data_processing_cumulative(survey_df)
     fig = px.area(data,
-                  x="date_of_report",
+                  x="date_of_report_day",
                   y="cumulative_number_of_reports", color_discrete_sequence=["white"],
                   width=600,  # width of the graph
                   height=300)  # height of the graph
