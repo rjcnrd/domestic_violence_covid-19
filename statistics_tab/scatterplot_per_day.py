f4ada6d33f56f209203c2949b7639a19f5668c82
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-08 15:40:14 +0200

    simplifying function

--- statistics_tab/scatterplot_per_day.py
+++ statistics_tab/scatterplot_per_day.py
@@ -15,7 +15,7 @@ def add_ranking(survey_df):
     survey_df.date_of_report = pd.to_datetime(survey_df.date_of_report, format='%d/%m/%Y %I:%M %p')
     survey_df["date_of_report_day"] = survey_df.date_of_report.dt.strftime('%Y/%m/%d')
     survey_df["row_number"] = np.arange(len(survey_df))
-    survey_df["ranking"] = survey_df.groupby(survey_df.date_of_report.dt.floor("d"))["row_number"].rank(ascending=True)
+    survey_df["ranking"] = survey_df.groupby(survey_df.date_of_report_day)["row_number"].rank(ascending=True)
     survey_df.date_of_report_day = pd.to_datetime(survey_df.date_of_report_day)
     return survey_df
 
