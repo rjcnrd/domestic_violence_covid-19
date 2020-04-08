43be3164149d8372012c88792c504d4795d1594e
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-08 15:36:30 +0200

    small variable change name

--- statistics_tab/scatterplot_per_day.py
+++ statistics_tab/scatterplot_per_day.py
@@ -12,13 +12,12 @@ def add_ranking(survey_df):
     :param survey_df: df with a column "date_of_report"
     :returns: a df that has a new column "rankings"
     """
-    df_new = survey_df
-    df_new.date_of_report = pd.to_datetime(df_new.date_of_report, format='%d/%m/%Y %I:%M %p')
-    df_new["date_of_report_day"] = df_new.date_of_report.dt.strftime('%Y/%m/%d')
-    df_new["row_number"] = np.arange(len(df_new))
-    df_new["ranking"] = df_new.groupby(df_new.date_of_report.dt.floor("d"))["row_number"].rank(ascending=True)
-    df_new.date_of_report_day = pd.to_datetime(df_new.date_of_report_day)
-    return df_new
+    survey_df.date_of_report = pd.to_datetime(survey_df.date_of_report, format='%d/%m/%Y %I:%M %p')
+    survey_df["date_of_report_day"] = survey_df.date_of_report.dt.strftime('%Y/%m/%d')
+    survey_df["row_number"] = np.arange(len(survey_df))
+    survey_df["ranking"] = survey_df.groupby(survey_df.date_of_report.dt.floor("d"))["row_number"].rank(ascending=True)
+    survey_df.date_of_report_day = pd.to_datetime(survey_df.date_of_report_day)
+    return survey_df
 
 
 def draw_scatterplot_per_day(survey_df):
