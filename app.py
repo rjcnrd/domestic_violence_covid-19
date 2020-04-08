976fd140477baff494dbae34f7af01f4242fbc29
Roberta Conrad <rjcnrd@gmail.com>
2020-04-08 11:56:39 +0200

    importing bootstrap

--- app.py
+++ app.py
@@ -2,13 +2,15 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 
 from layout import create_layout
 
 #import pandas as pd
 import logging
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [dbc.themes.LUX]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
