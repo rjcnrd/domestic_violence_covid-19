b91de7d6b588b31e5ebe0b97b0d7ef9093e34f5d
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-08 11:51:16 +0200

    titles for the overview tab

--- overview_tab/overview.py
+++ overview_tab/overview.py
@@ -6,13 +6,23 @@ from overview_tab.cumulative_nb_report import cumulative_graph
 
 def create_overview_tab(df, postal_code_df, map_threshold):
     tab_content = html.Div([
-        html.Div([
-            dcc.Graph(figure=map_graph(df, postal_code_df, map_threshold)),
-            ],
+        html.Div(children=[
+            html.Div(children=
+                     html.Span("Number of incidents reported per postal code", className="graph-heading-span"),
+                     className="graph-heading"
+                     ),
+            html.Div([
+                dcc.Graph(figure=map_graph(df, postal_code_df, map_threshold)),
+            ])],
             style={'width': '48%', 'display': 'inline-block'}),
-        html.Div([dcc.Graph(figure=cumulative_graph(df))],
-                 style={'width': '48%', 'align': 'right', 'display': 'inline-block'})])
+        html.Div(children=[
+            html.Div(children=[
+                html.Div(children=
+                         html.Span("Cumulative number of incidents reported", className="graph-heading-span"),
+                         className="graph-heading"
+                         ),
+                html.Div([dcc.Graph(figure=cumulative_graph(df))],
+                         )])],
+            style={'width': '48%', 'align': 'right', 'display': 'inline-block'})])
 
     return tab_content
-
-
