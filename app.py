e799ec029e56f9a938b8647ef6f06f010a48cb9d
Roberta Conrad <rjcnrd@gmail.com>
2020-04-08 12:02:32 +0200

    cleaning up app.py

--- app.py
+++ app.py
@@ -5,23 +5,10 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 
 from layout import create_layout
-
-#import pandas as pd
 import logging
 
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 external_stylesheets = [dbc.themes.LUX]
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-#df = pd.read_csv('dummy-data.csv')
-#logging.info(dummy_data_df)
-#print("DEBUG",df)
-
-colors = {
-    'background': '#00a0dd',
-    'text': '#fff'
-}
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
