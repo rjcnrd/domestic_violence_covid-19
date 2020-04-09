792cdcc8b10c49b160363db3018d8a046d4f2b15
Roberta Conrad <rjcnrd@gmail.com>
2020-04-09 19:37:14 +0200

    adding random font sizing

--- testimonials_tab/testimonials.py
+++ testimonials_tab/testimonials.py
@@ -5,8 +5,11 @@ import dash_bootstrap_components as dbc
 
 import random
 import pandas as pd
+import math
+import numpy as np
 
 MESSAGES_DISPLAYED = 9
+
 """ 
 def get_messages(survey_df):
     all_reports = survey_df.written_report.tolist()
@@ -14,6 +17,26 @@ def get_messages(survey_df):
     first_x_reports_after_shuffle = all_reports[0:MESSAGES_DISPLAYED]
     return first_x_reports_after_shuffle """
 
+def get_fontsize(testimonial):
+    """
+    this function determines a font size in rem
+    default on page is: 0.875
+    max is: 1.5
+    min is: 0.75
+    if text_length falls between limits rem is calculated by fitted exponential function
+    random gaussian noise is added with mean = calculated size with a sd. of 0.1
+    """
+    text_length = len(testimonial)
+    if text_length > 800: 
+        size = np.random.normal(0.75,0.1) 
+    elif text_length < 50: 
+        size = np.random.normal(1.5,0.1) 
+    else:
+        size =  round(1.5*(math.exp(-0.0009*text_length)),2)
+        size = np.random.normal(size,0.1) 
+    
+    stringified = str(size)+"rem"
+    return stringified
 
 def get_messages(survey_df):
     """
@@ -28,9 +51,15 @@ def create_testimonials_tab(survey_df):
     list_of_messages = get_messages(survey_df)
     tab_content =         dbc.Row([
             dbc.Col([
-                html.P(list_of_messages[0]),
-                html.P(list_of_messages[1]),
-                html.P(list_of_messages[2])
+                html.P(list_of_messages[1],style={
+                    'font-size': get_fontsize(list_of_messages[1])
+                    }),
+                html.P(list_of_messages[2],style={
+                'font-size': get_fontsize(list_of_messages[2])
+                }),
+                html.P(list_of_messages[3],style={
+                'font-size': get_fontsize(list_of_messages[3])
+                })
             ], md=4),
 
 
