6860999818a035a05322eff37476c584c6b0bd3f
Amélie Meurer <amelie.meurer@hec.edu>
2020-04-09 19:22:49 +0200

    adding space for survivors report on app

--- overview_tab/overview.py
+++ overview_tab/overview.py
@@ -8,7 +8,7 @@ from overview_tab.cumulative_nb_report import cumulative_graph
 
 def create_overview_tab(df, postal_code_df, map_threshold):
     tab_content = dbc.Row([
-        #Column 1: Left Side
+        # Column 1: Left Side
         dbc.Col([
             html.H4(children=html.Span("Number of incidents reported per postal code", className="graph-heading-span"),
                     className="graph-heading"
@@ -18,13 +18,28 @@ def create_overview_tab(df, postal_code_df, map_threshold):
                     df, postal_code_df, map_threshold))
             ])], md=6
         ),
-        #Column 2: Right Side
+        # Column 2: Right Side
         dbc.Col([
-            html.H4(children=html.Span("Cumulative number of incidents reported", className="graph-heading-span"),
-                    className="graph-heading"
-                    ),
-            html.Div([dcc.Graph(figure=cumulative_graph(df))
-                      ])
+            html.Div(
+                [html.H4(children=html.Span("Cumulative number of incidents reported", className="graph-heading-span"),
+                         className="graph-heading"
+                         ),
+                 html.Div([dcc.Graph(figure=cumulative_graph(df))
+                           ])
+                 ]),
+            html.Div([
+                html.H4(children=html.Span("Survivors report", className="graph-heading-span"),
+                        className="graph-heading"
+                        ),
+                dbc.Row([
+                    dbc.Col(html.Div("One of two columns")),
+                    dbc.Col(html.Div("One of two columns"))
+                ]),
+                dbc.Row([
+                    dbc.Col(html.Div("One of two columns")),
+                    dbc.Col(html.Div("One of two columns"))
+                ])
+            ])
         ], md=6)
     ])
 
