99550dc34ccdf1860446c38350c14165fe35ac5f
Amélie Meurer <amelie.meurer@hec.edu>
2020-05-01 15:28:46 +0200

    add row for new graphs

--- statistics_tab/statistics.py
+++ statistics_tab/statistics.py
@@ -1,16 +1,23 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 
 from statistics_tab.scatterplot_per_day import draw_scatterplot_per_day
 
 
 def create_statistics_tab(survey_df):
-    tab_content = [
-        html.H4(children=
-                 html.Span('This is how many of you got in touch with us in the past month', className="graph-heading-span"),
-                 className="graph-heading"),
-        dcc.Graph(
-            figure=draw_scatterplot_per_day(survey_df),
-            responsive=True
-        )]
+    tab_content = html.Div([
+        dbc.Row([
+            html.H4(children=html.Span('This is how many of you got in touch with us in the past month',
+                                       className="graph-heading-span"),
+                    className="graph-heading"),
+            html.Div([dcc.Graph(figure=draw_scatterplot_per_day(survey_df), responsive=True
+                                )])]),
+        dbc.Row(
+            # for Roberta
+        ),
+        dbc.Row(
+            # for Amélie
+        )])
+
     return tab_content
