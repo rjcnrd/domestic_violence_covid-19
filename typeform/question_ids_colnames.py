3cab17f975ec8229069a0eb17ca8fbe64ce643c7
Roberta Conrad <rjcnrd@gmail.com>
2020-05-08 17:56:56 +0200

    adding overview file to map IDs to survey questions

---
+++ typeform/question_ids_colnames.py
@@ -0,0 +1,101 @@
+#OVERVIEW OF ALL QUESTION IDS 
+
+"id": "Xa2T0bcZ5VnY",
+"title": "Do you identify as a:",
+
+"id": "QUtA6k0gcrZf",
+"title": "How old are you? ",
+
+"id": "ZNZyNVwMtwID",
+"title": "How would you identify your ethnicity"
+
+"id": "azlKRkvdcc7O",
+"title": "Who are you in lockdown with"
+
+"id": "Vrzj2uv0i29C",
+"title": "Where are you based during lockdown"
+
+"id": "Xh2W1d6FhXyK",
+"title": "Are you feeling safe at home"
+
+"id": "t9EaTsONnJfR",
+"title": "How has your feeling of safety at home changed"
+
+"id": "OaujoVSY6NaS",
+"title": "Why do you think this is"
+
+"id": "ASolp4v3adZK",
+"title": "Are you doing okay during lockdown - mental helath"
+
+"id": "erHBE6gKxycf",
+"title": "Have you experienced any of these negative experiences"
+
+"id": "ASQmq4h3vxRt",
+"title": "Have you had any of these positive"
+
+"id": "KEGztC8iYfyy",
+"title": "Are you currently working?"
+
+"id": "kWb7LUjdJyhb",
+"title": "How has COVID-19 made you feel about your ifnancial situation"
+
+"id": "g9goY6hIxmC9",
+"title": "How much of the housework do you do"
+
+"id": "jG2wzvAmOWxq",
+"title": "Has your time spent on caring changed"
+
+"id": "HNqA7BK7ZDFl",
+"title": "Is there anything else you would like to share about your experience "
+
+"id": "hXM65Xe75JCt",
+"title": "EMAIL"
+
+
+#OVERVIEW OF ALL COLNAMES
+
+
+
+colnames = ["id",
+    "gender",
+    "age",
+    "ethnicity",
+    "live_alone",
+    "live_friends",
+    "live_partner",
+    "live_parents",
+    "live_siblings",
+    "live_otherfamily",
+    "live_children",
+    "live_other",
+    "location",
+    "safety_scale",
+    "safety_change",
+    "safety_why",
+    "mental_scale",
+    "mental_problems_repetitive",
+    "mental_problems_lonely",
+    "mental_problems_stressed",
+    "mental_problems_anxious",
+    "mental_problems_worriedhealth",
+    "mental_problems_scaredfuture",
+    "mental_problems_relationship",
+    "mental_problems_other",
+    "mental_problems_none",
+    "positive_impact_support_housework",
+    "positive_impact_support_childcare",
+    "positive_impact_connect",
+    "positive_impact_leisure",
+    "positive_impact_wfh",
+    "positive_impact_other",
+    "positive_impact_none",
+    "work_situation",
+    "work_situation_other",
+    "financial_situation",
+    "housework_amount",
+    "housework_change",
+    "testimonial",
+    "start_date",
+    "submit_date",
+    "n_id"
+]
