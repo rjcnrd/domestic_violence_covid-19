528b3a39e7a3d02584ac731ad500950cb36373bc
Roberta Conrad <rjcnrd@gmail.com>
2020-05-11 13:15:44 +0200

    further changes on notebook for graph1 data. replaced the fake data for real data

--- statistics_tab/scatterplot_per_day.py
+++ statistics_tab/scatterplot_per_day.py
@@ -5,37 +5,45 @@ import pandas as pd
 COLORSCALE = ["white", "#d80052"]
 FAMILY = "PT Sans"
 
+graph1_df = pd.read_csv("./typeform/data_graph1.csv")
+
 
 def add_ranking(survey_df):
     """
     adds a rank between (1, #of entries / day) to every report
-    :param survey_df: df with a column "date_of_report"
+    :param survey_df: df with a column "submitted_at"
     :returns: a df that has a new column "rankings"
     """
-    survey_df.date_of_report = pd.to_datetime(survey_df.date_of_report, format='%d/%m/%Y %I:%M %p')
-    survey_df["date_of_report_day"] = survey_df.date_of_report.dt.strftime('%Y/%m/%d')
+    survey_df.submitted_at = pd.to_datetime(survey_df.submitted_at)
+    survey_df["submitted_at_day"] = survey_df.submitted_at.dt.strftime('%Y/%m/%d')
     survey_df["row_number"] = np.arange(len(survey_df))
-    survey_df["ranking"] = survey_df.groupby(survey_df.date_of_report_day)["row_number"].rank(ascending=True)
-    survey_df.date_of_report_day = pd.to_datetime(survey_df.date_of_report_day)
+    survey_df["ranking"] = survey_df.groupby(survey_df.submitted_at_day)["row_number"].rank(ascending=True)
+    survey_df.submitted_at_day = pd.to_datetime(survey_df.submitted_at_day)
     return survey_df
 
 
 def draw_scatterplot_per_day(survey_df):
+    #PATCH
+    survey_df = graph1_df
+
     df = add_ranking(survey_df)
     figure = go.Figure(data=go.Scatter(
-        x=df.date_of_report_day,
+        x=df.submitted_at_day,
         y=df.ranking,
         hovertemplate="%{text}" +
                       "<extra></extra>",
-        text=df.written_report,
+
+
+     
+        text=df.testimonial,
         hoverlabel=dict(
             bgcolor="#00a0dd"),
         mode='markers',
         marker=dict(
-            #size=40,
+            #size=10,
             color= df.display_testimonial,
             colorscale=COLORSCALE,
-            opacity=0.8
+            opacity=0.7
         )
 
     ),
